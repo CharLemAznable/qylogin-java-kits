084832b468f5e76f83590e11da0f738ee4cdf5e9
CharLemAznable <545541819@qq.com>
2019-09-09 18:07:59 +0800

    Upgrade, some refactor.

--- src/test/java/com/github/charlemaznable/qylogin/CookieValueTest.java
+++ src/test/java/com/github/charlemaznable/qylogin/CookieValueTest.java
@@ -1,7 +1,6 @@
 package com.github.charlemaznable.qylogin;
 
 import lombok.val;
-import org.joda.time.DateTime;
 import org.junit.jupiter.api.Test;
 
 import static com.github.charlemaznable.core.codec.Json.unJson;
@@ -12,13 +11,13 @@ public class CookieValueTest {
     @Test
     public void testCookieValue() {
         val jsonString = "{\"UserId\":\"a\",\"Name\":\"b\",\"Avatar\":\"c\",\"CsrfToken\":\"d\",\"Expired\":\"2019-03-05T23:33:59.029596352+08:00\"}";
-        CookieValue cookieValue = unJson(jsonString, CookieValue.class);
+        val cookieValue = unJson(jsonString, CookieValue.class);
 
         assertEquals("a", cookieValue.getUserID());
         assertEquals("b", cookieValue.getName());
         assertEquals("c", cookieValue.getAvatar());
         assertEquals("d", cookieValue.getCsrfToken());
-        DateTime expired = cookieValue.getExpired();
+        val expired = cookieValue.getExpired();
         assertEquals(2019, expired.getYear());
         assertEquals(3, expired.getMonthOfYear());
         assertEquals(5, expired.getDayOfMonth());

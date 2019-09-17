50d6c74212d2e8e76e1bfa92eeaded9efb1a253d
CharLemAznable <545541819@qq.com>
2019-09-17 17:40:45 +0800

    Some refactor.

--- src/main/java/com/github/charlemaznable/qylogin/spring/QyLoginConfiguration.java
+++ src/main/java/com/github/charlemaznable/qylogin/spring/QyLoginConfiguration.java
@@ -5,6 +5,7 @@ import com.github.charlemaznable.core.spring.ComplexImport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,7 +19,7 @@ public class QyLoginConfiguration implements WebMvcConfigurer {
     private QyLoginInterceptor qyLoginInterceptor;
 
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(qyLoginInterceptor).addPathPatterns("/**");
+    public void addInterceptors(@NonNull InterceptorRegistry registry) {
+        registry.addInterceptor(qyLoginInterceptor);
     }
 }

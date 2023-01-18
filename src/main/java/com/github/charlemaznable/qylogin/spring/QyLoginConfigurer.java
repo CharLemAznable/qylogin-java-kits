012a3d8f9207c21710b3570d7a1a1989474c8aa5
CharLemAznable <545541819@qq.com>
2023-01-19 01:51:56 +0800

    Some refactor.

--- src/main/java/com/github/charlemaznable/qylogin/spring/QyLoginConfigurer.java
+++ src/main/java/com/github/charlemaznable/qylogin/spring/QyLoginConfigurer.java
@@ -2,20 +2,28 @@ package com.github.charlemaznable.qylogin.spring;
 
 import com.github.charlemaznable.core.spring.ElvesImport;
 import com.github.charlemaznable.qylogin.config.QyLoginConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Nonnull;
-import java.util.function.Supplier;
 
 import static com.github.charlemaznable.configservice.ConfigFactory.getConfig;
-import static com.github.charlemaznable.core.spring.SpringContext.getBeanOrAutowire;
+import static com.github.charlemaznable.core.lang.Condition.nullThen;
 
 @Configuration
 @ElvesImport
 public class QyLoginConfigurer implements WebMvcConfigurer {
+
+    private final QyLoginConfig qyLoginConfig;
+
+    @Autowired
+    public QyLoginConfigurer(@Nullable QyLoginConfig qyLoginConfig) {
+        this.qyLoginConfig = nullThen(qyLoginConfig, () -> getConfig(QyLoginConfig.class));
+    }
 
     @Override
     public void addInterceptors(@Nonnull InterceptorRegistry registry) {
@@ -24,14 +32,6 @@ public class QyLoginConfigurer implements WebMvcConfigurer {
 
     @Bean("com.github.charlemaznable.qylogin.spring.QyLoginInterceptor")
     public QyLoginInterceptor qyLoginInterceptor() {
-        return new QyLoginInterceptor(qyLoginConfig());
-    }
-
-    private QyLoginConfig qyLoginConfig() {
-        return getBeanOrAutowire(QyLoginConfig.class, defaultQyLoginConfigSupplier());
-    }
-
-    private Supplier<QyLoginConfig> defaultQyLoginConfigSupplier() {
-        return () -> getConfig(QyLoginConfig.class);
+        return new QyLoginInterceptor(qyLoginConfig);
     }
 }
